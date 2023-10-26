2a7ea2149f10a51d51afd48bb3ce1a43ea13cf21
SarahThird <sarahthird@gmail.com>
2023-10-25 20:05:14 -0700

    Creating a little script / executable that will allow me to manually trigger clock syncs.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace UpdateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start a command prompt     
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.Verb = "runas";
+            process.Start();
+
+            // Trigger a resync 
+            process.StandardInput.WriteLine("w32tm /resync");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+        }
+    }
+}
